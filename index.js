6a5931ed586c0e67170bafa34943d62ccbd7e483
Allen Mordukhaev <allenmor93@gmail.com>
2022-07-21 16:09:10 -0400

    commit tree 0bb833846bb7f272ea11229c614ec4ed6841f26a

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+let array = []
 // Code your solutions in this file
+const writeCards = function(name) {
+    for(let i = 0; i < name.length; i++){
+        array.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`)
+    }
+    return array
+}
+
+const countDown = function(ten) {
+    for(let i = 0; i < 11; i++){
+        console.log(i)
+    }
+}
+
